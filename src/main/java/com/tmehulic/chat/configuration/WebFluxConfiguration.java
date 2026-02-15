bd4d00fa6a4616b95fb64b8d014817ea2f645702
Tarik Mehulić <tmehulic.dev@gmail.com>
2026-02-15 21:37:47 +0100

    Add CORS mapping

---
+++ src/main/java/com/tmehulic/chat/configuration/WebFluxConfiguration.java
@@ -0,0 +1,17 @@
+package com.tmehulic.chat.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfiguration implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
